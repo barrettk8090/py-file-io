f6fea5206dc3c4decc24ee6db9dc64c122e7f73b
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-07 12:03:04 -0600

    intro to lambda

--- students.py
+++ students.py
@@ -6,8 +6,13 @@ with open("students.csv") as file:
         student = {"name":name, "house":house}
         students.append(student)
 
-def get_name(student):
-    return student["name"]
-
-for student in sorted(students, key=get_name):
+# lambda function, anonymous function
+for student in sorted(students, key=lambda student: student['name']):
     print(f"{student['name']} is in {student['house']}")
+
+# # this is the same as:
+# def get_name(student):
+#     return student['name']
+
+# for student in sorted(students, key=get_name):
+#     print(f"{student['name']} is in {student['house']}")
