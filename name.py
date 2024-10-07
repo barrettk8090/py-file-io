2170375819dc2623073e2dd4c49cd99f021de902
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-07 08:04:02 -0600

    basic name file

--- name.py
+++ name.py
@@ -1,2 +1,7 @@
-name = input("What's your name? ")
-print(f"hello, {name}")
+names = []
+
+for _ in range(3):
+    names.append(input("What's your name? "))
+
+for name in sorted(names):
+    print(f"hello, {name}")
