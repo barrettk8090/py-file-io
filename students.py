f859085f8bc36de24fad3415bd7850df3f53694f
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-07 12:39:27 -0600

    csv.writer

--- students.py
+++ students.py
@@ -1,11 +1,9 @@
-import csv
+import csv 
 
-students = []
+name = input("What's your name? ")
+home = input("Where's your home? ")
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row["name"], "home": row["home"]})
-
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['home']}")
+# append mode
+with open("students.csv", "a") as file:
+    writer = csv.writer(file)
+    writer.writerow([name, home])
