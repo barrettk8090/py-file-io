68fd97c9b019ae679df15698ecddccaf8b1d7da0
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-07 08:14:53 -0600

    basic using with keyword

--- names.py
+++ names.py
@@ -1,7 +1,5 @@
 name = input("What's your name? ")
 
-# open a file and write to it, but this recreates and overwrites the file.
-# file = open("names.txt", "w")
-file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+# using with statements - autocloses the file
+with open("names.txt", "a") as file: 
+    file.write(f"{name}\n")
