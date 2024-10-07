6ff6d7df1efe66e9b5d0a20059f5989760d6168f
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-07 08:34:42 -0600

    basic reading file

--- names.py
+++ names.py
@@ -1,5 +1,8 @@
-name = input("What's your name? ")
+with open("names.txt", "r") as file:
+    lines = file.readlines()
 
-# using with statements - autocloses the file
-with open("names.txt", "a") as file: 
-    file.write(f"{name}\n")
+# basic implementation would print each name on a new line
+# can either use , end="" after "line" in the print statement 
+# or use line.rstrip()
+for line in lines:
+    print("hello,", line.rstrip())
