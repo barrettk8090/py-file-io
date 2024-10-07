9304832099e824cd5bfcc4bb8d7774705b6c3d47
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-07 08:52:27 -0600

    parsing CSVs basics

--- names.py
+++ names.py
@@ -2,5 +2,8 @@ names = []
 
 # if opening a file to read, don't need "r" (default behavior)
 with open("names.txt") as file:
-    for line in sorted(file):
-        print("hello,", line.rstrip())
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {names}")
